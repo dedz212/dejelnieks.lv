2aff11735c1b47ef88d6e50b69985f07706ef0e7
dedz212 <dejelniekskonta@gmail.com>
2024-02-29 00:42:49 +0200

    1709160132
    Updated content, added JSTimer

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,4 +1,5 @@
-console.log(`1707667648 - Mobile Responsive Fix`)
+console.log(`1709160132 - Updated content, added JSTimer`)
+
 /*  TOOGLE THEME */
 const tct = document.querySelectorAll("#tct");
 tct.forEach(function(tct) {
@@ -47,3 +48,72 @@ tct.forEach(function(tct) {
 		alert('LAD not working');
 	}
 });
+
+/* CONTENT MAIN */
+fetch('./assets/content.json')
+	.then(response => response.json())
+	.then(data => {
+		const musicSection = document.getElementById('music');
+		const webSection = document.getElementById('web');
+
+		data.music.forEach(item => {
+			const div = document.createElement('div');
+			div.className = `cn ${item.div}`;
+
+			const a = document.createElement('a');
+			a.className = 'tavslaiks';
+			a.href = item.href;
+
+			const divFace = document.createElement('div');
+			divFace.className = `face ${item.div2}`;
+
+			const divTavslaiks2 = document.createElement('div');
+			divTavslaiks2.className = 'tavslaiks2';
+
+			const h1 = document.createElement('h1');
+			h1.className = 'tt';
+			h1.textContent = item.tt;
+			
+			const p = document.createElement('p');
+			p.className = 'rednew';
+			p.textContent = item.rednew;
+
+			divTavslaiks2.appendChild(h1);
+			if (item.rednew !== "") {
+				divTavslaiks2.appendChild(p);
+			}
+			a.appendChild(divFace);
+			a.appendChild(divTavslaiks2);
+			div.appendChild(a);
+			musicSection.appendChild(div);
+		});
+
+		data.web.forEach(item => {
+			const div = document.createElement('div');
+			div.className = 'cn';
+
+			const a = document.createElement('a');
+			a.href = item.href;
+
+			const h1 = document.createElement('h1');
+			h1.className = 'tt2';
+			h1.textContent = item.tt;
+
+			const p = document.createElement('p');
+			p.className = 'ttt';
+			p.textContent = item.ttt;
+
+			const pRednew = document.createElement('p');
+			pRednew.className = 'rednew';
+			pRednew.textContent = item.rednew;
+
+			a.appendChild(h1);
+			a.appendChild(p);
+			if (item.rednew !== "") {
+				a.appendChild(pRednew);
+			}
+			div.appendChild(a);
+			webSection.appendChild(div);
+		});
+	})
+	.catch(error => console.error('Error fetching JSON: ', error));
